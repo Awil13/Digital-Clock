5322e240e44282940b57f16fa5b1aaca6816462c
Ashlee Gonzales <ash.wilson40@gmail.com>
2023-06-18 15:00:55 -0700

    Color change code fixed
    Background can now change with time but it only changes when browser refreshes

--- Clock.js
+++ Clock.js
@@ -1,24 +1,25 @@
 let hrs = document.getElementById("hrs")
 let min = document.getElementById("min")
 let sec = document.getElementById("sec")
+let currentTime = new Date();
+ 
+let change = ()=>{
+    if (currentTime.getHours() >= 5 && currentTime.getHours() < 12){
+    document.body.className = "day";
+    } else if( currentTime.getHours() >= 12  && currentTime.getHours()< 18 ){
+        document.body.className = "afternoon";
+    } else {
+        document.body.className = "night";
+    }
+};
 
-setInterval(()=>{
+let double = ()=>{
     let currentTime = new Date();
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
-},1000);
-
- let change = ()=>{
-    let time = new Date();
-    if (time.getHours()>18 && time.getHours()<6){
-        document.getElementById("afternoon").id = "night";
-    } else if (time.getHours()>5 && time.getHours<12){
-        document.getElementById("afternoon").id = "day";
-    } else{
-        document.getElementById("afternoon").id = "afternoon";
-    }
 };
 
-
-
+setInterval(double,change,1000)
+change();
+double();
